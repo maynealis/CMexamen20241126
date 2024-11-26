96d25698b25eb875c3820484b73d207037c2f47a
mayne <104829121+maynealis@users.noreply.github.com>
2024-11-26 10:08:52 +0100

    Levanto servidor

--- triangulo.py
+++ triangulo.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer , BaseHTTPRequestHandler
 from jinja2 import Template
 import urllib.parse
 
-def genera_html_base(title):
+def get_html_base(title):
     """Devuelve un string con el html correspondiente al header.
 
     Args:
@@ -21,8 +21,7 @@ def genera_html_base(title):
         </head>
     """
     
-
-def genera_index(user_initials = ""):
+def get_html_index(user_initials = ""):
     """Devuelve un string con el html correspondiente a la página principal con un formulario para introducir base y altura de un triángulo.
 
     Args:
@@ -38,19 +37,19 @@ def genera_index(user_initials = ""):
         </h1>
         <form action="/resultado" method="post">
             <label for="base">Base:</label><br>
-            <input type="text" id="base" name="base"><br>
+            <input type="text" id="base" name="base" required><br>
             <label for="lado2">Altura:</label><br>
-            <input type="text" id="altura" name="altura"><br>
+            <input type="text" id="altura" name="altura" required><br>
             <input type="submit" value="Calcular">
         </form>
 
     </body>
     </html>
     """
-    html_content = genera_html_base("Calcula área") + html_body
+    html_content = get_html_base("Calcula área") + html_body
     return html_content
 
-def genera_resultado(base, altura):
+def get_html_result(base, altura):
     """Devuelve un string con el html correspondiente a la página resultado donde visualizar el cálculo del área de un triángulo.
 
     Args:
@@ -66,8 +65,50 @@ def genera_resultado(base, altura):
     </body>
     </html>
     """
-    html_content = genera_html_base("Resultado") + html_body
+    html_content = get_html_base("Resultado") + html_body
     return html_content
+
+    
+class CustomBaseHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def write_html(self, file_html):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(file_html, 'utf-8'))
+    
+
+    def do_GET(self):
+        print ("I got a GET request.")
+        self.write_html(get_html_index("CM"))
+        
+
+    def do_POST(self):
+        print("POST request")
+        content_length = int(self.headers.get('Content-Length'))
+        post_data = self.rfile.read(content_length)
+        params = urllib.parse.parse_qs(post_data.decode('utf-8'))
+        print(params)
+        base = float(params['base'][0])
+        altura = float(params['altura'][0])
+
+        self.write_html(get_html_result(base, altura))
+    
+
+
+if __name__ == "__main__":
+    server_name = "localhost"
+    server_port = 3001
+    with HTTPServer((server_name, server_port), CustomBaseHTTPRequestHandler) as httpd:
+        print(f"Servidor en http://{server_name}:{server_port}")
+        try: 
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            # keyboard interrupt in terminal (Ctrl+C)
+            print("\nDeteniendo el servidor...")
+            httpd.shutdown()
+            print("Servidor parado")
+    
 
     
 
